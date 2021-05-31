508f57bd582381e968c389d4bbff416b717f0e33
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-06-01 00:00:23 +0900

    EDIT | Concatenate Variables

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextHeight : EditText = findViewById(R.id.editTextHeight);
+        val editTextWeight : EditText = findViewById(R.id.editTextWeight);
+        val buttonCalculate : Button = findViewById(R.id.buttonCalculate);
     }
 }
