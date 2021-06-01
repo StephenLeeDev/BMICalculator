d8f2e45d7a78a46b770da50d109c717b33746c84
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-06-02 00:06:27 +0900

    ADD | Add Input Implementation

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextHeight : EditText = findViewById(R.id.editTextHeight);
-        val editTextWeight : EditText = findViewById(R.id.editTextWeight);
-        val buttonCalculate : Button = findViewById(R.id.buttonCalculate);
+        val editTextHeight : EditText = findViewById(R.id.editTextHeight)
+        val editTextWeight : EditText = findViewById(R.id.editTextWeight)
+        val buttonCalculate : Button = findViewById(R.id.buttonCalculate)
+
+        buttonCalculate.setOnClickListener {
+            val height = editTextHeight.text.toString()
+            val weight = editTextWeight.text.toString()
+
+            if (height.isEmpty()) {
+                Toast.makeText(this, "Please Enter Height", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (weight.isEmpty()) {
+                Toast.makeText(this, "Please Enter Weight", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
